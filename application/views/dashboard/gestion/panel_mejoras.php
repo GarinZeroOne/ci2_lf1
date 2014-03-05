62b302f6a5e3e750bd28f892cf30c15be295d251
G.G <garincuentas@gmail.com>
2014-03-05 14:41:28 +0100

    mejoras diseño

--- application/views/dashboard/gestion/panel_mejoras.php
+++ application/views/dashboard/gestion/panel_mejoras.php
@@ -7,42 +7,50 @@
         <div class="row">
             <div class="col-md-4">
                 
-                <div class="profile-nav alt">
+                <div class="profile-nav alt" style="float:left;">
                     <section class="panel">
                         <div class="user-heading alt clock-row terques-bg">
-                            <h1>Mecánicos</h1>
+                            <h1><i class="fa fa-wrench"></i> Mecánicos</h1>
                             <p class="text-left">Nivel 5</p>
                             <p class="text-left" style="font-size:12px;line-height:14px;">
                                 Los mecanicos se encargaran de conseguir que el monoplaza rinda mas de lo normal.Para esto, le agregan al motor una pieza especial llamada STIKI. </br>
                                 Por cada nivel que  subas esta mejora, reducirás un 5% el precio de los STIKIS de dinero y de puntos, hasta un máximo del 50%.
                             </p>
+                            <div align="right" style="margin-top:10px"><button class="btn btn-success btn-xs" type="button"><i class="fa fa-level-up"></i> Aumentar nivel: 120.000 €</button></div>
                         </div>
                         
 
-                        <ul class="clock-category">
-                            <li>
-                                <a class="active" href="#">
-                                    <i class="ico-clock2"></i>
-                                    <span>Clock</span>
-                                </a>
+                        <ul class="nivel-mejoras">
+                            <li <?php if(true):?> class="active1" <?php endif;?> >
+                                <span class="nivel">1</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class="ico-alarm2 "></i>
-                                    <span>Alarm</span>
-                                </a>
+                            <li  <?php if(true):?> class="active2" <?php endif;?> >
+                                <span class="nivel">2</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class="ico-stopwatch"></i>
-                                    <span>Stop watch</span>
-                                </a>
+                            <li <?php if(true):?> class="active3" <?php endif;?> >
+                                <span class="nivel">3</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class=" ico-clock2 "></i>
-                                    <span>Timer</span>
-                                </a>
+                            <li <?php if(true):?> class="active4" <?php endif;?> >
+                                <span class="nivel">4</span>
+                            </li>
+                            <li <?php if(true):?> class="active5" <?php endif;?> >
+                                <span class="nivel">5</span>
+                            </li>
+                            <li <?php if(true):?> class="active6" <?php endif;?> >
+                                <span class="nivel">6</span>
+                            </li>
+                            <li <?php if(true):?> class="active7" <?php endif;?> >
+                                <span class="nivel">7</span>
+                            </li>
+                            <li <?php if(true):?> class="active8" <?php endif;?> >
+                                <span class="nivel">8</span>
+                            </li>
+
+                            <li <?php if(true):?> class="active9" <?php endif;?> >
+                                <span class="nivel">9</span>
+                            </li>
+                            <li <?php if(true):?> class="active10" <?php endif;?> >
+                                <span class="nivel">10</span>
                             </li>
                         </ul>
 
@@ -53,39 +61,51 @@
 
             <div class="col-md-4">
                 
-                <div class="profile-nav alt">
+                <div class="profile-nav alt" style="float:left;">
                     <section class="panel">
                         <div class="user-heading alt clock-row terques-bg">
-                            <h1>December 14</h1>
-                            <p class="text-left">2014, Friday</p>
-                            <p class="text-left">7:53 PM</p>
+                            <h1><i class="fa fa-gears"></i> Ingenieros</h1>
+                            <p class="text-left">Nivel 1</p>
+                            <p class="text-left" style="font-size:12px;line-height:14px;">
+                                Los ingenieros son los encargados de estudiar los STIKIS. Son capaces de optimizar el rendimiento de los STIKIS de manera que produzcan aún más beneficio </br>
+                                Al subir de nivel los ingenieros, aumentará tanto el beneficio los stikis de piloto como los stikis de dinero.
+                            </p>
+                            <div align="right" style="margin-top:10px"><button class="btn btn-success btn-xs" type="button"><i class="fa fa-level-up"></i> Aumentar nivel: 120.000 €</button></div>
+
                         </div>
                         
 
-                        <ul class="clock-category">
-                            <li>
-                                <a class="active" href="#">
-                                    <i class="ico-clock2"></i>
-                                    <span>Clock</span>
-                                </a>
+                        <ul class="nivel-mejoras">
+                            <li <?php if(true):?> class="active1" <?php endif;?> >
+                                <span class="nivel">1</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class="ico-alarm2 "></i>
-                                    <span>Alarm</span>
-                                </a>
+                            <li  <?php if(true):?> class="active2" <?php endif;?> >
+                                <span class="nivel">2</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class="ico-stopwatch"></i>
-                                    <span>Stop watch</span>
-                                </a>
+                            <li <?php if(true):?> class="active3" <?php endif;?> >
+                                <span class="nivel">3</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class=" ico-clock2 "></i>
-                                    <span>Timer</span>
-                                </a>
+                            <li <?php if(true):?> class="active4" <?php endif;?> >
+                                <span class="nivel">4</span>
+                            </li>
+                            <li <?php if(true):?> class="active5" <?php endif;?> >
+                                <span class="nivel">5</span>
+                            </li>
+                            <li <?php if(true):?> class="active6" <?php endif;?> >
+                                <span class="nivel">6</span>
+                            </li>
+                            <li <?php if(true):?> class="active7" <?php endif;?> >
+                                <span class="nivel">7</span>
+                            </li>
+                            <li <?php if(true):?> class="active8" <?php endif;?> >
+                                <span class="nivel">8</span>
+                            </li>
+
+                            <li <?php if(true):?> class="active9" <?php endif;?> >
+                                <span class="nivel">9</span>
+                            </li>
+                            <li <?php if(true):?> class="active10" <?php endif;?> >
+                                <span class="nivel">10</span>
                             </li>
                         </ul>
 
@@ -96,39 +116,51 @@
 
             <div class="col-md-4">
                 
-                <div class="profile-nav alt">
+                <div class="profile-nav alt" style="float:left;">
                     <section class="panel">
                         <div class="user-heading alt clock-row terques-bg">
-                            <h1>December 14</h1>
-                            <p class="text-left">2014, Friday</p>
-                            <p class="text-left">7:53 PM</p>
+                            <h1><i class="fa fa-money"></i> Publicistas</h1>
+                            <p class="text-left">Nivel 1</p>
+                            <p class="text-left" style="font-size:12px;line-height:14px;">
+                                Los publicistas son los encargados de conseguir patrocinios y publicidad, un adecuado nivel de publicistas hará aumentar tus ingresos rápidamente.</br>
+                                Los publicistas aumentan el TOTAL de tus beneficios ecónomicos del GP.
+                            </p>
+
+                            <div align="right" style="margin-top:10px"><button class="btn btn-success btn-xs" type="button"><i class="fa fa-level-up"></i> Aumentar nivel: 120.000 €</button></div>
                         </div>
                         
 
-                        <ul class="clock-category">
-                            <li>
-                                <a class="active" href="#">
-                                    <i class="ico-clock2"></i>
-                                    <span>Clock</span>
-                                </a>
+                        <ul class="nivel-mejoras">
+                            <li <?php if(true):?> class="active1" <?php endif;?> >
+                                <span class="nivel">1</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class="ico-alarm2 "></i>
-                                    <span>Alarm</span>
-                                </a>
+                            <li  <?php if(true):?> class="active2" <?php endif;?> >
+                                <span class="nivel">2</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class="ico-stopwatch"></i>
-                                    <span>Stop watch</span>
-                                </a>
+                            <li <?php if(true):?> class="active3" <?php endif;?> >
+                                <span class="nivel">3</span>
                             </li>
-                            <li>
-                                <a href="#">
-                                    <i class=" ico-clock2 "></i>
-                                    <span>Timer</span>
-                                </a>
+                            <li <?php if(true):?> class="active4" <?php endif;?> >
+                                <span class="nivel">4</span>
+                            </li>
+                            <li <?php if(true):?> class="active5" <?php endif;?> >
+                                <span class="nivel">5</span>
+                            </li>
+                            <li <?php if(true):?> class="active6" <?php endif;?> >
+                                <span class="nivel">6</span>
+                            </li>
+                            <li <?php if(true):?> class="active7" <?php endif;?> >
+                                <span class="nivel">7</span>
+                            </li>
+                            <li <?php if(true):?> class="active8" <?php endif;?> >
+                                <span class="nivel">8</span>
+                            </li>
+
+                            <li <?php if(true):?> class="active9" <?php endif;?> >
+                                <span class="nivel">9</span>
+                            </li>
+                            <li <?php if(true):?> class="active10" <?php endif;?> >
+                                <span class="nivel">10</span>
                             </li>
                         </ul>
 
