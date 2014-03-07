fec1ba2926cb689748acb02535d3ad3cf71faa63
mikelatsg <mikelatsg@gmail.com>
2014-03-07 11:44:30 +0100

    Ficha pilotos

--- application/views/dashboard/mercado/piloto.php
+++ application/views/dashboard/mercado/piloto.php
@@ -20,7 +20,7 @@
                 <section class="panel">
                     <header class="panel-heading">
                         Ficha piloto
-                        
+
                     </header>
                     <div class="panel-body">                       
                         <div class="row">
@@ -49,57 +49,59 @@
                                         <div class="col-md-12 col-xs-12">
                                             <div>
                                                 <h4 class="text-center">Datos LF1</h4>
-                                                <ul>
-                                                    <li>Valor actual : <?php echo $piloto->getValorActual(true); ?></li>
-                                                    <li>Valor anterior : <?php echo $piloto->getValorAnterior(true); ?></li>
-                                                    <li>Cambio valor : <?php if ($piloto->getCambioValor() > 0):
-   ?>
-                                                        <i class="fa fa-angle-double-up" style="color: rgb(90, 157, 29); font-size: 16px;"></i>
-                                                        <span style="color: rgb(90, 157, 29);">
+                                                <ul class="nav nav-pills nav-stacked">
+                                                    <li style="vertical-align: middle;"><a href="javascript:;"><i class="fa fa-credit-card" style="margin-right: 4px"></i> Valor actual : <span style="margin: 0 0 0;" class="badge  pull-right r-activity"><?php echo $piloto->getValorActual(true); ?></span></a></li>
+                                                    <li style="vertical-align: middle;"><a href="javascript:;"><i class="fa fa-credit-card" style="margin-right: 4px"></i> Valor anterior : <span style="margin: 0 0 0;" class="badge  pull-right r-activity"><?php echo $piloto->getValorAnterior(true); ?></span></a></li>
+                                                    <li style="vertical-align: middle;"><a href="javascript:;"><i class="fa fa-tachometer" style="margin-right: 4px"></i> Cambio valor : <?php if ($piloto->getCambioValor() > 0): ?>
+                                                        <span class="badge label-success2 pull-right r-activity" style="margin: 0 0 0;">
+                                                            <i class="fa fa-angle-double-up" style="color: rgb(90, 157, 29);"></i>
+
                                                             <?php elseif ($piloto->getCambioValor() < 0) : ?>
-                                                                <i class="fa fa-angle-double-down" style="color: rgb(255, 0, 0); font-size: 16px;"></i>
-                                                                <span style="color: rgb(255, 0, 0);">
+                                                                <span class="badge label-danger pull-right r-activity" style="margin: 0 0 0;">
+                                                                    <i class="fa fa-angle-double-down" style="color: rgb(255, 0, 0);"></i>                                                                
                                                                     <?php
                                                                 else:
                                                                     ?>
-                                                                    <i class="fa fa-minus" font-size: 16px;></i>
-                                                                <?php
-                                                                endif;
-                                                                echo $piloto->getCambioValor(true) . "( " . $piloto->getCambioPorcentaje(true) . " )";
-                                                                ?>
-                                                            </span>
+                                                                    <span class="badge  pull-right r-activity" style="margin: 0 0 0;">
+                                                                        <i class="fa fa-angle-double-right" ></i>
+                                                                    <?php
+                                                                    endif;
+                                                                    echo $piloto->getCambioValor(true) . "( " . $piloto->getCambioPorcentaje(true) . " )";
+                                                                    ?>
+                                                                        </a>
+                                                                </span>
 
-                                                    </li>
-                                                    <li>Precio alquiler : <?php echo $piloto->getPrecioAlquiler(true); ?></li>
-                                                    <li>Valor maximo : <?php echo $piloto->getValorMax(true); ?></li>
-                                                    <li>Valor minimo : <?php echo $piloto->getValorMin(true); ?></li>
-                                                    <li>Puntos manager conseguidos : <?php echo $piloto->getPuntosConseguidos(); ?></li>
-                                                    <li>Dinero ganado : <?php echo $piloto->getDineroConseguido(true); ?></li>
-                                                </ul>
-                                            </div>
-                                            <div class="btn-group-justified">                                                 
-                                                <a class="btn btn-primary" href="<?php echo site_url() . 'mercado/ficharPiloto/' . $piloto->getIdPiloto(); ?>">Fichar</a>
-                                                <a class="btn btn-primary" href="<?php echo site_url() . 'mercado/alquilarPiloto/' . $piloto->getIdPiloto(); ?>">Alquilar</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </section>
-                            </div>
+                                                                </li>
+                                                                <li style="vertical-align: middle;"><a href="javascript:;"><i class="fa fa-credit-card" style="margin-right: 4px"></i> Precio alquiler : <span style="margin: 0 0 0;" class="badge  pull-right r-activity"><?php echo $piloto->getPrecioAlquiler(true); ?></span></a></li>
+                                                                <li style="vertical-align: middle;"><a href="javascript:;"><i class="fa fa-eur" style="margin-right: 4px"></i> Valor maximo : <span style="margin: 0 0 0;" class="badge  pull-right r-activity"><?php echo $piloto->getValorMax(true); ?></span></a></li>
+                                                                <li style="vertical-align: middle;"><a href="javascript:;"><i class="fa fa-eur" style="margin-right: 4px"></i> Valor minimo : <span style="margin: 0 0 0;" class="badge  pull-right r-activity"><?php echo $piloto->getValorMin(true); ?></span></a></li>
+                                                                <li style="vertical-align: middle;"><a href="javascript:;"><i class="fa fa-trophy" style="margin-right: 4px"></i> Puntos manager conseguidos : <span style="margin: 0 0 0;" class="badge label-warning pull-right r-activity"><?php echo $piloto->getPuntosConseguidos(); ?></span></a></li>
+                                                                <li style="vertical-align: middle;"><a href="javascript:;"> <i class="fa fa-eur" style="margin-right: 4px"></i> Dinero ganado : <span style="margin: 0 0 0;" class="badge  pull-right r-activity"><?php echo $piloto->getDineroConseguido(true); ?></span></a></li>
+                                                                </ul>
+                                                                </div>
+                                                                <div class="btn-group-justified">                                                 
+                                                                    <a class="btn btn-primary" href="<?php echo site_url() . 'mercado/ficharPiloto/' . $piloto->getIdPiloto(); ?>">Fichar</a>
+                                                                    <a class="btn btn-primary" href="<?php echo site_url() . 'mercado/alquilarPiloto/' . $piloto->getIdPiloto(); ?>">Alquilar</a>
+                                                                </div>
+                                                                </div>
+                                                                </div>
+                                                                </section>
+                                                                </div>
 
-                            <div class="col-md-8">
-                                <section class="panel">                                    
-                                    <div class="col-md-12" id='grafica' data='<?php echo $piloto->getIdPiloto(); ?>'>
-                                    </div>
-                                </section>
-                            </div>
+                                                                <div class="col-md-8">
+                                                                    <section class="panel">                                    
+                                                                        <div class="col-md-12" id='grafica' data='<?php echo $piloto->getIdPiloto(); ?>'>
+                                                                        </div>
+                                                                    </section>
+                                                                </div>
 
-                        </div>
+                                                                </div>
 
-                    </div>
-                </section>
-            </div>
-        </div>
-        <!-- page end-->
-    </section>
-</section>
-<!--main content end-->
+                                                                </div>
+                                                                </section>
+                                                                </div>
+                                                                </div>
+                                                                <!-- page end-->
+                                                                </section>
+                                                                </section>
+                                                                <!--main content end-->
